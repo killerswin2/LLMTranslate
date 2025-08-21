a75dd05e8d8c611e921444733daf7c7fe9af9b5f
Killerswin2 <thecakebecamethelie@yahoo.com>
2025-08-21 15:48:53 -0500

    Add project files.

---
+++ Tasking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+namespace LLMTranslate
+{
+    
+    public class Tasking
+    {
+        private static List<Thread> _WorkerThreads = new List<Thread>();
+        private static ConcurrentQueue<int> _IndexQueue = new ConcurrentQueue<int>();
+        public static void ControllerFunction()
+        {
+
+        }
+        public static void WorkerFunction()
+        {
+
+        }
+    }
+}
