0303a9675d61e5f43bdf39b5790c98f462d883d8
Killerswin2 <thecakebecamethelie@yahoo.com>
2025-08-23 13:12:02 -0500

    added post method for creating new server

--- Program.cs
+++ Program.cs
@@ -1,16 +1,70 @@
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
 namespace LLMTranslate
 {
     public class Program
     {
+        public static List<string> originalLines = new List<string>();
+        public static List<string> translatedLines = new List<string>();
+        private static ServerTasking tasking;
+
+        public static void FillOriginalLines()
+        {
+            if (File.Exists("LLMTranslateLines.txt"))
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                byte[] fileBytes = File.ReadAllBytes("LLMTranslateLines.txt");
+                Encoding encoding = Encoding.GetEncoding("Shift-JIS");
+
+                string decodedText = encoding.GetString(fileBytes);
+                StringReader reader = new StringReader(decodedText);
+
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (line != String.Empty)
+                    {
+                        originalLines.Add(line);
+                        translatedLines.Add(String.Empty);
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
         public static void Main(string[] args)
         {
+            tasking = new ServerTasking(originalLines.Count);
             // handle commandline args
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+            app.MapPost("/Add", async (HttpContext context) =>
+            {
+                if(context.Request.HasJsonContentType())
+                {
+                    ServerInfo? info;
+                    StreamReader reader = new StreamReader(context.Request.Body, Encoding.UTF8);
+                    string Jsonstring = await reader.ReadToEndAsync();
+                    JsonNode? JsonRoot = JsonNode.Parse(Jsonstring);
+                    JsonNode serverInfo = JsonRoot["ServerInfo"];
+                    JsonNode serverOpts = JsonRoot["Options"];
+                    if (serverInfo != null )
+                    {
+                        info = JsonSerializer.Deserialize<ServerInfo>(serverInfo);
+                        //tasking.AddServer(info, serverOpts);
+                    }
+                    return Results.Ok();
+                }
+                return Results.BadRequest();
+            });
 
             app.Run();
         }
+
+        
     }
 }
--- ServerTasking.cs
+++ ServerTasking.cs
@@ -10,10 +10,44 @@ namespace LLMTranslate
         private List<Thread> _WorkerThreads = new List<Thread>();
         private List<ServerInfo> _Servers = new List<ServerInfo>();
         private ConcurrentQueue<int> _IndexQueue = new ConcurrentQueue<int>();
-        public void MainServerFunction()
+        
+
+        public ServerTasking(int workSize)
+        {
+            for (int i = 0; i < workSize; i++)
+            {
+                _IndexQueue.Enqueue(i);
+            } 
+                
+        }
+
+        public CancellationTokenSource? AddServer(ServerInfo serverInfo, JsonNode serverOpts)
+        {
+            CancellationTokenSource source = new CancellationTokenSource();
+            if (serverInfo.Type == "vLLM")
+            {
+                
+                _WorkerThreads.Add(new Thread(new ThreadStart(() => WorkervLLMServerFunction(source.Token, serverInfo, serverOpts))));
+                return source;
+            }
+            if(serverInfo.Type == "Ollama")
+            {
+                _WorkerThreads.Add(new Thread(new ThreadStart(() => WorkerOllamaServerFunction(source.Token, serverInfo, serverOpts))));
+                return source;
+            }
+            if(serverInfo.Type == "Llama")
+            {
+                _WorkerThreads.Add(new Thread(new ThreadStart(() => WorkerLlamaServerFunction(source.Token, serverInfo, serverOpts))));
+                return source;
+            }
+            return null;
+        }
+
+        public void RemoveServer(CancellationToken cancellationToken, ServerInfo info)
         {
 
         }
+        
         public void WorkerOllamaServerFunction(CancellationToken cancellationToken, ServerInfo info, JsonNode serverOptions)
         {
             OllamaServerOptions? options = JsonSerializer.Deserialize<OllamaServerOptions>(serverOptions);
@@ -25,8 +59,10 @@ namespace LLMTranslate
                 bool success = _IndexQueue.TryDequeue(out result);
                 if (success)
                 {
-                    string prompt = String.Empty;
+                    
+                    string prompt = Program.originalLines[result];
                     string output = server.ExecuteGenerator(prompt);
+                    Program.translatedLines[result] = output;
                 }
             }
         }
